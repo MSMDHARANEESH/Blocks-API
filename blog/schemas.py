29c27c890d43fdb87cbe862feb47d4d7c92a705f
MSMDHARANEESH <msmdharaneesh003@gmail.com>
2024-02-13 00:30:37 +0530

    first

---
+++ blog/schemas.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel
+from typing import List,Optional
+
+
+class Blog(BaseModel):
+    title:str
+    body:str
+
+
+class User(BaseModel):
+    name:str
+    email:str
+    password:str
+
+
+class ShowUser(BaseModel):
+    name:str
+    email:str
+    blogs:List[Blog]
+    
+
+class ShowBlog(BaseModel):
+    title:str
+    body:str
+    creator:ShowUser
+    # for new version config orm_mode not needed
+    # class Config():
+    #     orm_mode=True
+
+
+class Login(BaseModel):
+    username:str
+    password:str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: Optional[str] = None       
